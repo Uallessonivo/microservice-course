d8177dd159255586709c487b42c1252f7805a40f
Uallesson Nunes Ivo <uallessons@gmail.com>
2022-03-05 06:53:52 -0300

    Authentication and Authorization done!

--- hrgateway/src/main/java/io/github/uallessonivo/hrgateway/config/AppConfig.java
+++ hrgateway/src/main/java/io/github/uallessonivo/hrgateway/config/AppConfig.java
@@ -1,16 +1,23 @@
 package io.github.uallessonivo.hrgateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 public class AppConfig {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey("MY-SECRET-KEY");
+        converter.setSigningKey(secret);
         return converter;
     }
 
